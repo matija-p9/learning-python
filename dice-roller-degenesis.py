7c6371f1e9f8c8e990931ed421ede68306b62336
matija-p9 <59355738+matija-p9@users.noreply.github.com>
2020-04-10 19:51:13 +0200

    Add files via upload

---
+++ dice-roller-degenesis.py
@@ -0,0 +1,68 @@
+import random
+
+print("-------------------------------")
+print("DEGENESIS DICE ROLLER BY MATIJA")
+print("-------------------------------")
+print("       ENTER 'Q' TO QUIT")
+print(" -----------------------------")
+print(" ENTER NUMBER OF DIES TO ROLL:")
+
+counter = 0
+while True:
+    print("")
+    rollInput = input("")
+    if rollInput == 'q' or rollInput == 'Q':
+        break
+    try:
+        rollInput = int(rollInput)
+    except:
+        for digit in range(len(str(rollInput))):
+            print("-", end='')
+        print("\nNo. Try again.")
+        continue
+    if int(rollInput) >= 100:
+        for digit in range(len(str(rollInput))):
+            print("-", end='')
+        print("\nNo. Try again.")
+        continue
+    elif int(rollInput) < 0:
+        for digit in range(len(str(rollInput))):
+            print("-", end='')
+        print("\nNo. Try again.")
+        continue
+    diceRolls = []
+    successes = []
+    triggers = 0
+    counter += 1
+    for die in range(rollInput):
+        roll = random.randint(1, 6)
+        diceRolls.append(roll)
+        if roll > 3:
+            successes.append(roll)
+        if roll == 6:
+            triggers += 1
+
+    diceRolls = sorted(diceRolls, reverse=True)
+    successes = sorted(successes, reverse=True)
+
+    for digit in range(len(str(rollInput))):
+            print("-", end='')
+    print("\nRoll #", counter, ": ", diceRolls, sep='')
+    print("-----------------")
+
+    if not successes:
+        print("Success | Trigger")
+        print("--------|--------")
+        print("      0 | 0      ")
+    elif len(successes) < 10:
+        print("Success | Trigger")
+        print("--------|--------")
+        print(f"      {len(successes)} | {triggers}      ")
+    else:
+        print("Success | Trigger")
+        print("--------|--------")
+        print(f"     {len(successes)} | {triggers}      ")
+    print("-----------------")
+
+    if diceRolls.count(1) > len(successes):
+        print("CRITICAL FAILURE!")
